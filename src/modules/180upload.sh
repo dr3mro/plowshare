dr3mro/plowshare

commit f4cc4a7e0207a34d29947984d29c6975bfa4283f

180upload (probe): add file size

src/modules/180upload.sh
@@ -217,9 +217,15 @@ MODULE_180UPLOAD_PROBE_OPTIONS=""
 
     REQ_OUT=c
 
+    # Note: all info parsed from HTML comments on the page
     if [[ $REQ_IN = *f* ]]; then
-        parse_tag 'center nowrap' b <<< "$PAGE" && \
-            REQ_OUT="${REQ_OUT}f"
+        parse_tag 'center nowrap' 'b' <<< "$PAGE" && REQ_OUT="${REQ_OUT}f"
+    fi
+
+    if [[ $REQ_IN = *s* ]]; then
+        FILE_SIZE=$(parse_tag 'Size:' 'small' <<< "$PAGE") && \
+            FILE_SIZE=${FILE_SIZE#(} && FILE_SIZE=${FILE_SIZE% bytes)} && \
+            REQ_OUT="${REQ_OUT}s"
     fi
 
     echo $REQ_OUT

commit 009016f1b9a71d9be626363267a26ecc2fbbda19

180upload (probe): actually output file size
Error introduced in commit f4cc4a7e0207a34d29947984d29c6975bfa4283f
Fixes issue 1150

src/modules/180upload.sh
@@ -219,7 +219,7 @@ MODULE_180UPLOAD_PROBE_OPTIONS=""
     if [[ $REQ_IN = *s* ]]; then
         FILE_SIZE=$(parse_tag 'Size:' 'small' <<< "$PAGE") && \
             FILE_SIZE=${FILE_SIZE#(} && FILE_SIZE=${FILE_SIZE% bytes)} && \
-            REQ_OUT="${REQ_OUT}s"
+            echo "$FILE_SIZE" && REQ_OUT="${REQ_OUT}s"
     fi
 
     echo $REQ_OUT