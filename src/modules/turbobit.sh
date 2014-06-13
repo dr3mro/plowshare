dr3mro/plowshare

commit 131c465ee26419c1f9ebb894bf3f8550cfb5aa33

turbobit (download, probe): fix link detection

src/modules/turbobit.sh
@@ -123,8 +123,10 @@ turbobit_download() {
 
     PAGE=$(curl -c "$COOKIE_FILE" -b 'user_lang=en' "$URL") || return
 
-    # <h1>This document was not found in System</h1>
-    match 'was not found' "$PAGE" && return $ERR_LINK_DEAD
+    # This document was not found in System
+    # File was not found. It could possibly be deleted.
+    # File not found. Probably it was deleted.
+    matchi '\(file\|document\)\(was \)\? not found' "$PAGE" && return $ERR_LINK_DEAD
     [ -n "$CHECK_LINK" ] && return 0
 
     # Download xyz. Free download without registration from TurboBit.net
@@ -443,7 +445,10 @@ turbobit_probe() {
 
     PAGE=$(curl -b 'user_lang=en' "$URL") || return
 
-    match 'was not found'  "$PAGE" && return $ERR_LINK_DEAD
+    # This document was not found in System
+    # File was not found. It could possibly be deleted.
+    # File not found. Probably it was deleted.
+    matchi '\(file\|document\)\(was \)\? not found' "$PAGE" && return $ERR_LINK_DEAD
     REQ_OUT=c
 
     if [[ $REQ_IN = *f* ]]; then

commit 1d98c7f954f43f9780be0180e06b68c357e1000b

turbobit (download, probe): fix detection of dead files
Regression introduced in 131c465ee26419c1f9ebb894bf3f8550cfb5aa33
Fixes issue 1158

src/modules/turbobit.sh
@@ -128,7 +128,7 @@ turbobit_download() {
     # This document was not found in System
     # File was not found. It could possibly be deleted.
     # File not found. Probably it was deleted.
-    matchi '\(file\|document\)\(was \)\? not found' "$PAGE" && return $ERR_LINK_DEAD
+    matchi '\(file\|document\)\( was\)\? not found' "$PAGE" && return $ERR_LINK_DEAD
 
     # Download xyz. Free download without registration from TurboBit.net
     FILE_NAME=$(echo "$PAGE" | parse '<title>' \
@@ -444,7 +444,7 @@ turbobit_probe() {
     # This document was not found in System
     # File was not found. It could possibly be deleted.
     # File not found. Probably it was deleted.
-    matchi '\(file\|document\)\(was \)\? not found' "$PAGE" && return $ERR_LINK_DEAD
+    matchi '\(file\|document\)\( was\)\? not found' "$PAGE" && return $ERR_LINK_DEAD
     REQ_OUT=c
 
     if [[ $REQ_IN = *f* ]]; then